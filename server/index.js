61337ae6ea93066baecd3e494d9a55da1a8bbf23
23f3003596 <23f3003596@ds.study.iitm.ac.in>
2025-11-15 11:23:02 +0530

    Add explicit OPTIONS preflight handler for CORS

--- server/index.js
+++ server/index.js
@@ -57,6 +57,21 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// Handle preflight requests explicitly
+app.options('*', cors({
+  origin: function (origin, callback) {
+    console.log('🔍 OPTIONS preflight from origin:', origin);
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
